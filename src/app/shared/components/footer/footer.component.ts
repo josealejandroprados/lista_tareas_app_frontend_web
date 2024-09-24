3d146796a54fa3e8ba2b04162e84a1bafb2f0e4a
josealejandroprados <josealejandroprados@gmail.com>
2024-09-24 14:46:07 -0300

    implementando el footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  fecha = new Date();
+  texto1 = '@'+this.fecha.getFullYear() + ' ' + 'Ing. José A. Prados' + ', IT Developer Full Stack';
+  enlace_linkedIn = 'https://www.linkedin.com/in/jos%C3%A9-alejandro-prados-70930b169/';
+  enlace_github = 'https://github.com/josealejandroprados?tab=repositories';
+
+  openWhatsApp(){
+    const phoneNumber = '543814410958';
+    const message = '';
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, '_blank');
+  }
+
 }
