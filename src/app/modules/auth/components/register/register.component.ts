95f3167d333e39cdd439d4e8a82aca8504d155c7
josealejandroprados <josealejandroprados@gmail.com>
2024-09-25 11:17:47 -0300

    agregando los metodos login y register en servicio auth, implementando registro de usuario en component register

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  formRegister = new FormGroup({
+    'nombre': new FormControl('',Validators.required),
+    'apellido': new FormControl('',Validators.required),
+    'email': new FormControl('',[Validators.required, Validators.email]),
+    'password': new FormControl('',Validators.required),
+  });
+
+  constructor(
+    private router:Router,
+    private auth:AuthService
+  ){}
+
+  registrar(){
+    if(this.formRegister.valid){
+      // llamo al servicio
+      this.auth.register(this.formRegister.value).subscribe(resultado => {
+        if(resultado.message=='exito'){
+          alert('registro realizado con exito');
+          this.router.navigate(['login']);
+        }
+        else{
+          alert('ha ocurrido un error');
+        }
+      });
+    }
+  }
+
+
+  //getters
+  get nombre(){
+    return this.formRegister.get('nombre') as FormControl;
+  }
+  get apellido(){
+    return this.formRegister.get('apellido') as FormControl;
+  }
+  get email(){
+    return this.formRegister.get('email') as FormControl;
+  }
+  get password(){
+    return this.formRegister.get('password') as FormControl;
+  }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000';
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  // registrar usuario
+  register(user:any){
+    return this.http.post<any>(`${this.baseUrl}/register`,user);
+  }
+
+  // iniciar sesión/login
+  login(user:any){
+    return this.http.post<any>(`${this.baseUrl}/login`,user);
+  }
 }
